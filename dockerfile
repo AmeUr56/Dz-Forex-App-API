7b0844a54de9e7f155120e9d1a17318edc2c3dfe
Ameur56 <boughershaouari@gmail.com>
2025-12-09 00:00:39 +0100

    Fixed Relative Paths Errors

--- dockerfile
+++ dockerfile
@@ -5,7 +5,8 @@ WORKDIR /app
 COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
+RUN playwright install --with-deps chromium
 
 EXPOSE 9000
 
-CMD ["uvicorn", "app.run:app", "--host", "0.0.0.0", "--port", "9000", "--workers", "2"]
+CMD ["uvicorn", "run:app", "--host", "0.0.0.0", "--port", "9000", "--workers", "2"]
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 import subprocess
 import json
+import os
 
 from main import cache_decorator
 
@@ -20,14 +21,18 @@ exchanges_router = APIRouter(prefix="/exchanges", tags=["exchanges"])
 @exchanges_router.get("/",
         summary="Get Exchanges",
         description="Get Multiple Currencies Exchange.")
-@cache_decorator(expire=86_400) # Daily
+#@cache_decorator(expire=86_400) # Daily
 async def get_exchanges():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    CRAWLER_DIR = os.path.join(BASE_DIR, "ExchangeCrawler")
+    DATA_FILE = os.path.join(BASE_DIR, "ExchangeCrawler", "data.json")
+
     subprocess.run(
-        ["python", "-m", "scrapy", "crawl", "exchanges", "-O", "data.json"], 
-        cwd="ExchangeCrawler"
+        ["python", "-m", "scrapy", "crawl", "exchanges", "-O", DATA_FILE],
+        cwd=CRAWLER_DIR
     )
 
-    with open("ExchangeCrawler/data.json", "r") as file:
-        data = json.load(file)
+    with open(DATA_FILE, "r") as f:
+        data = json.load(f)
 
     return data
